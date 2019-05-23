ac195c81d605ee079e7b2e5edfd99613ad06d6fe
andrei <andrew000@list.ru>
2019-05-24 01:19:58 +0300

    initial commit

---
+++ src/test/java/com/andreymironov/iactestback/IactestbackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.andreymironov.iactestback;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IactestbackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
